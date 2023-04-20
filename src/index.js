3f78777c307883016e5883d14b17e2d7cc64dde5
Phone Khaing Hein <25-00209@dat.com>
2023-04-20 16:22:05 +0630

    add item count

--- src/index.js
+++ src/index.js
@@ -7,11 +7,12 @@ import mealDishesCounter from './modules/itemCtr.js';
 import commentsCounter from './modules/commentsCtr.js';
 
 // Call the countMealDishes function to display the total number of meal dishes on the homepage
-mealDishesCounter.countMealDishes();
-commentsCounter.countComments();
+// mealDishesCounter.countMealDishes();
+// commentsCounter.countComments();
 
 document.addEventListener('DOMContentLoaded', () => {
   fetchData();
+  mealDishesCounter();
   if (!localStorage.getItem('appId')) {
     getAppId();
   }
--- src/modules/itemCtr.js
+++ src/modules/itemCtr.js
@@ -1,26 +1,33 @@
 import { baseApiUrl } from './api.js';
 
-const mealDishesCounter = (() => {
-  const countMealDishes = async () => {
-    try {
-      const response = await fetch(baseApiUrl);
-      const data = await response.json();
-      let totalMealDishes = 0;
-      data.categories.forEach((category) => {
-        totalMealDishes += category.meals.length;
-      });
-      const counterElement = document.querySelector('#meal-dish-counter');
-      counterElement.innerText = totalMealDishes;
-    } catch (error) {
-      console.error('An error occurred while counting meal dishes', error);
-      const counterElement = document.querySelector('#meal-dish-counter');
-      counterElement.innerText = 'N/A';
-    }
-  };
+// const mealDishesCounter = (() => {
+//   const countMealDishes = async () => {
+//     try {
+//       const response = await fetch(baseApiUrl);
+//       const data = await response.json();
+//       let totalMealDishes = 0;
+//       data.categories.forEach((category) => {
+//         totalMealDishes += category.meals.length;
+//       });
+//       const counterElement = document.querySelector('#meal-dish-counter');
+//       counterElement.innerText = totalMealDishes;
+//     } catch (error) {
+//       console.error('An error occurred while counting meal dishes', error);
+//       const counterElement = document.querySelector('#meal-dish-counter');
+//       counterElement.innerText = 'N/A';
+//     }
+//   };
 
-  return {
-    countMealDishes,
-  };
-})();
+//   return {
+//     countMealDishes,
+//   };
+// })();
+
+const mealDishesCounter = async () => {
+  const response = await fetch(baseApiUrl);
+  const json = await response.json();
+  const { categories } = json;
+  document.getElementById('itemCount').innerText = categories.length;
+};
 
 export default mealDishesCounter;
