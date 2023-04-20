884ddcd9d9b9d76af93a52d8a5b69ab264b63ce2
emhamza <imumairhamza@gmail.com>
2023-04-20 21:56:24 +0500

    Update files for make more professional

--- src/modules/category.js
+++ src/modules/category.js
@@ -1,4 +1,5 @@
 const involvementApiUrl = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/';
+const likesMap = JSON.parse(localStorage.getItem('likesMap')) || {};
 
 const renderCategory = (category) => {
   const categoryDiv = document.createElement('div');
@@ -17,7 +18,12 @@ const renderCategory = (category) => {
   // Display number of likes for this category
   const likesCount = document.createElement('p');
   likesCount.className = 'likes-count';
-  likesCount.textContent = `Likes: ${category.likes}`;
+
+  // Update the likesCount value with the value stored in the likesMap
+  const categoryId = category.idCategory;
+  const storedLikes = likesMap[categoryId] || 0;
+  likesCount.textContent = `Likes: ${storedLikes}`;
+
   categoryDiv.appendChild(likesCount);
 
   // Create buttons container for this category
@@ -27,7 +33,7 @@ const renderCategory = (category) => {
   // Create a like button for this category
   const likeButton = document.createElement('button');
   likeButton.className = 'btn btn-outline-danger like-button my-like-button my-2';
-  likeButton.dataset.categoryId = category.idCategory;
+  likeButton.dataset.categoryId = categoryId;
   likeButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12 21.35l-1.45-1.32C5.4 14.35 2 11.12 2 7.5 2 4.42 4.42 2 7.5 2c1.93 0 3.75.95 4.87 2.53C13.75 2.95 15.57 2 17.5 2 20.58 2 23 4.42 23 7.5c0 3.62-3.4 6.85-8.55 12.53L12 21.35z"/></svg>';
 
   // Add event listener to each like button
@@ -49,8 +55,11 @@ const renderCategory = (category) => {
 
       if (response.status === 201) {
         const likesCount = categoryDiv.querySelector('.likes-count');
-        const currentLikes = Number(likesCount.textContent.split(' ')[1]);
-        likesCount.textContent = `Likes: ${currentLikes + 1}`;
+        let currentLikes = Number(likesCount.textContent.split(' ')[1]);
+        currentLikes += 1;
+        likesCount.textContent = `Likes: ${currentLikes}`;
+        likesMap[categoryId] = currentLikes;
+        localStorage.setItem('likesMap', JSON.stringify(likesMap));
       }
     } catch (error) {
       console.error('Error recording like:', error);
